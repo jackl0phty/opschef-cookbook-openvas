1ccecc0dfeb54818e1878162a2744aaccae548be
Gerald L. Hevener Jr. M.S <hevenerg@marshall.edu>
2012-08-27 14:00:43 -0400

    Fixed metadata.rb for foodcritic compliance. Fixed template openvassd.conf.erb for foodcritic compliance.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,15 @@ license          "Apache 2.0"
 description      "Installs/Configures openvas"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.52"
-%w{ apt perl }.each do |cb|
+recipe           "openvas", "Default recipe. Does nothing by default."
+recipe           "openvas::client", "Installs repo & the OpenVAS client."
+recipe           "openvas::repo", "Installs the OpenVAS YUM repo."
+recipe           "openvas::greenbone_scan_configs", "Downloads greenbone scan configs."
+recipe           "openvas::nmap", "Install port scanner Nmap."
+recipe           "openvas::openvasknife", "Install Perl cmd-line utility openvasknife."
+recipe           "openvas::scan_single_target", "Used openvasknife to scan a target."
+recipe           "openvas::server", "Install the OpenVAS server."
+
+%w{ apt perl openvas }.each do |cb|
   depends cb
 end
